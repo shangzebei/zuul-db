0c8f56ddcf16e914a5d181cf209cfa78eeafb83c
shangzebei <917058965@qq.com>
2017-07-03 19:14:11 +0800

    ok

---
+++ src/main/webapp/js/home.js
@@ -0,0 +1,54 @@
+/**
+ * Created by shang-mac on 2017/7/2.
+ */
+$(document).ready(function () {
+    http.getAjax_clean("home/getAll", function (data) {
+        $('#table').bootstrapTable({
+            columns: [{
+                field: 'id',
+                title: 'ID'
+            }, {
+                field: 'title',
+                title: '标识符'
+            }, {
+                field: 'path',
+                title: '匹配路由'
+            },{
+                field: 'local',
+                title: '目标路由'
+            },{
+                field: 'ops',
+                title: '操作',
+                formatter: operateFormatter
+            }],
+            data: data
+        });
+    })
+});
+function operateFormatter(value, row, index) {
+    return [
+        '<button type="button" class="btn btn-primary zuul-btn" onclick="change('+index+')">修改目标</button>',
+        '<button type="button" class="btn btn-danger zuul-btn" onclick="del('+index+')">删  除</button>'
+    ].join('');
+}
+function save() {
+    var url = $("#url").val();
+    var local = $("#local").val();
+    var path = $("#path").val();
+    var from = new FormData();
+    from.append("title", url);
+    from.append("local", local)
+    from.append("path", path)
+    http.postAjax_clean("home/add", from, function (data) {
+        if (data.state == true) {
+            window.location.reload();
+        }
+
+    })
+}
+function del(i) {
+    alert(i);
+}
+function change(i) {
+
+}
