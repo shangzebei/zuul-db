46566134c36943651dcd35b68e25d361d46607a6
shang-mac <917058965@qq.com>
2017-07-03 22:13:19 +0800

    fix some bug

--- src/main/java/com/shang/zuul/controller/Home.java
+++ src/main/java/com/shang/zuul/controller/Home.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.zuul.filters.Route;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,22 +29,22 @@ public class Home {
     private ZuulService zuulService;
     @Autowired
     private HomeServie homeServie;
-    @GetMapping("/")
-    private String list(Model model) {
-        List<Route> routes = zuulService.getRoutes();
-        ArrayList<URLEntry> urlEntries = new ArrayList<>();
-        for (int i = 1; i <= routes.size(); i++) {
-            URLEntry urlEntry = new URLEntry();
-            Route route = routes.get(i - 1);
-            urlEntry.setId(Long.valueOf(i));
-            urlEntry.setTitle(route.getId());
-            urlEntry.setPath(route.getFullPath());
-            urlEntry.setLocal(route.getLocation());
-            urlEntries.add(urlEntry);
-        }
-        model.addAttribute("kk", urlEntries);
-        return "home";
-    }
+//    @GetMapping("/")
+//    private String list(Model model) {
+//        List<Route> routes = zuulService.getRoutes();
+//        ArrayList<URLEntry> urlEntries = new ArrayList<>();
+//        for (int i = 1; i <= routes.size(); i++) {
+//            URLEntry urlEntry = new URLEntry();
+//            Route route = routes.get(i - 1);
+//            urlEntry.setId(Long.valueOf(i));
+//            urlEntry.setTitle(route.getId());
+//            urlEntry.setPath(route.getFullPath());
+//            urlEntry.setLocal(route.getLocation());
+//            urlEntries.add(urlEntry);
+//        }
+//        model.addAttribute("kk", urlEntries);
+//        return "home";
+//    }
 
     @GetMapping("home/getAll")
     @ResponseBody
