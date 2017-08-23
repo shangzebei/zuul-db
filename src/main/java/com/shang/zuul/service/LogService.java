f06e10b0602fe99666d36ffbce70a85c4e347ba3
shangzebei <917058965@qq.com>
2017-08-23 11:52:47 +0800

    add response support

--- src/main/java/com/shang/zuul/service/LogService.java
+++ src/main/java/com/shang/zuul/service/LogService.java
@@ -7,13 +7,8 @@ import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 import org.springframework.stereotype.Component;
-import org.springframework.util.FileSystemUtils;
 
 import javax.servlet.http.HttpServletRequest;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.FileSystem;
 
 import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SEND_RESPONSE_FILTER_ORDER;
 
@@ -47,15 +42,8 @@ public class LogService extends ZuulFilter {
         RequestContext ctx = RequestContext.getCurrentContext();
         final HttpServletRequest request = ctx.getRequest();
         log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
-//        InputStream responseDataStream = ctx.getResponseDataStream();
-//        try {
-//            byte[] b=new byte[responseDataStream.available()];
-//            responseDataStream.read(b);
-//            log.info(new String(b,"UTF-8"));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
         messageWebsocket.sendMessage(new Message(1,String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString())));
+
 
         return null;
     }
